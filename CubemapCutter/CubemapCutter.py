6751d80b95023fd924f76a0bdefde3fa1463c7a9
Vladimir Kvitnevskiy <vkvitnevski@gmail.com>
2013-09-16 14:06:53 -0400

    Only outputs the slices with "meaningful" content.

--- CubemapCutter/CubemapCutter.py
+++ CubemapCutter/CubemapCutter.py
@@ -23,17 +23,27 @@ assert( in_sz[ind_large] / 4 == in_sz[ind_small] / 3 )
 cube_len = in_sz[ind_large] / 4
 
 #slice up the image. First index is X, second index is Y
-slices = [[Image.new( 'RGB', (cube_len, cube_len)) for x in xrange(in_sz[1] / cube_len)] for x in xrange(in_sz[0] / cube_len)] 
+slices = [[ [Image.new( 'RGB', (cube_len, cube_len)), 0] \
+					for x in xrange(in_sz[1] / cube_len)] \
+					for x in xrange(in_sz[0] / cube_len)] 
+slices_data = [[ 0 for x in xrange(in_sz[1] / cube_len)] for x in xrange(in_sz[0] / cube_len)] 
 
 for i in range(len( slices )):
 	for j in range( len(slices[i])):
-		slices[i][j] = im.crop( (i*cube_len, j*cube_len, (i+1)*cube_len, (j+1)*cube_len ))
+		slices[i][j][0] = im.crop( (i*cube_len, j*cube_len, (i+1)*cube_len, (j+1)*cube_len ))
 
 for i in range(len( slices )):
 	for j in range( len(slices[i])):
-		slices[i][j].save( (str(i) + str(j) + '.jpg'), 'JPEG' )
+		tmp_color = slices[i][j][0].getpixel( (0, 0) )
+		for pixel in slices[i][j][0].getdata():
+			if( pixel != tmp_color ):
+				slices[i][j][1] = 1
+				break
+
+for i in range(len( slices )):
+	for j in range( len(slices[i])):
+		if( slices[i][j][1] != 0 ):
+			slices[i][j][0].save( (str(i) + str(j) + '.png'), 'PNG' )
 
 
-#im.save( inp_str + '.' + 'jpg', 'JPEG' )
-
-print('Hello World')
+print( 'Done' )
